38137489b63b639f8c5b7e57f3510e5b17a795c2
schmi <schmi@DESKTOP>
2024-12-01 22:31:18 +0100

    Projektdateien hinzufügen.

---
+++ BlackJack/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BlackJack
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
