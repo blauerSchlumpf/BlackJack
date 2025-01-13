90964d7cef2fba0993acdd0a7d038f848b055ded
schmi <mail@bella-ella.de>
2025-01-13 14:47:41 +0100

    color graph

--- BlackJack/ViewModels/ChartViewModel.cs
+++ BlackJack/ViewModels/ChartViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using BlackJack.ViewModels;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using SkiaSharp; // Für die Farbangaben
 using System.Collections.ObjectModel;
+
 namespace BlackJack.ViewModels;
 public class ChartViewModel : ViewModelBase
 {
@@ -13,23 +16,29 @@ public class ChartViewModel : ViewModelBase
     {
         data = dataProvider;
 
-        // Binde die Daten an die Chart-Serien
+        // Binde die Daten an die Chart-Serien und setze Farben
         ChartData = new ObservableCollection<ISeries>
         {
             new LineSeries<int>
             {
                 Values = data.BudgetData,
-                Name = "Budget"
+                Name = "Budget",
+                Stroke = new SolidColorPaint(SKColors.LightBlue), // Linienfarbe
+                Fill = new SolidColorPaint(SKColors.LightBlue.WithAlpha(50)) // Füllfarbe (transparent)
             },
             new LineSeries<int>
             {
                 Values = data.BetData,
-                Name = "Einsatz"
+                Name = "Einsatz",
+                Stroke = new SolidColorPaint(SKColors.OrangeRed), // Linienfarbe
+                Fill = new SolidColorPaint(SKColors.OrangeRed.WithAlpha(50)) // Füllfarbe (transparent)
             },
             new LineSeries<int>
             {
                 Values = data.ProfitData,
-                Name = "Gewinn"
+                Name = "Gewinn",
+                Stroke = new SolidColorPaint(SKColors.LimeGreen), // Linienfarbe
+                Fill = new SolidColorPaint(SKColors.LimeGreen.WithAlpha(50)) // Füllfarbe (transparent)
             }
         };
     }
